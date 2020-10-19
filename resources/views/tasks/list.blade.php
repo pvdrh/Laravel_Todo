bec2e2dd98e35889c50d441299e2a8f8f93961e4
pvdrh <duyvpvnua@gmail.com>
2020-10-19 18:19:14 +0700

    fix

--- resources/views/tasks/list.blade.php
+++ resources/views/tasks/list.blade.php
@@ -71,25 +71,25 @@
                         <div class="col-sm-6">
                             <input type="text" name="name" id="task-name" class="form-control" value="{{ old('task') }}">
                         </div>
+                        
                     </div> 
                     <div class="form-group">
                         <label for="task-name" class="col-sm-3 control-label">Mô tả công việc</label>
                         <div class="col-sm-6">
                             <input type="text" name="content" id="task-content" class="form-control" value="{{ old('task') }}">
                     </div>
-                    <hr>
+                   
+                    </div> 
                     <div class="form-group">
                         <label for="task-name" class="col-sm-3 control-label">Thời hạn công việc</label>
                         <div class="col-sm-6">
                             <input type="text" name="deadline" id="task-deadline" class="form-control" value="{{ old('task') }}">
                     </div>
-                    
-                    </div> 
 
                     <!-- Add Task Button -->
                     <div class="form-group">
                         <div class="col-sm-offset-3 col-sm-6">
-                            <button type="submit" class="btn btn-default">
+                            <button  style="margin: 10px" type="submit" class="btn btn-default">
                                 <i class="fa fa-btn fa-plus"></i>Thêm công việc
                             </button>
                         </div>
